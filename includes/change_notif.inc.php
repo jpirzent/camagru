572177afe101fac67e2a4678d91e334bdcc2869a
James PIRZENTHAL <jpirzent@e4r14p7.wethinkcode.co.za>
2018-11-12 17:51:20 +0200

    change_notif

--- includes/change_notif.inc.php
+++ includes/change_notif.inc.php
@@ -1,17 +1,18 @@
 <?php
 
-    session_start();
+	session_start();
 
-    if (isset($_POST['submit']) && isset($_SESSION['u_uid']))
-    {
-        include_once 'dbh.inc.php';
+	if (isset($_POST['submit']) && isset($_SESSION['u_uid']))
+	{
+		include_once 'dbh.inc.php';
 
-        $uid = $_SESSION['u_uid'];
+		$uid = $_SESSION['u_uid'];
 
-        try
+		try
 		{
-			$sql = "SELECT * FROM users WHERE user_uid='$uid'";
+			$sql = "SELECT * FROM users WHERE user_uid=:username";
 			$check = $conn->prepare($sql);
+			$check->bindParam(":username", $uid);
 			$check->execute();
 			$row = $check->fetch(PDO::FETCH_ASSOC);
 		}
@@ -19,32 +20,48 @@
 		{
 			echo $var->getMessage();
 		}
-        if ($row)
-        {
-            if ($row['user_notif'] == 1)
-            {
-                $sql = "UPDATE users SET user_notif='0' WHERE user_uid='$uid'";
-				$stmt = $conn->prepare($sql);
-				$stmt->execute();
-				$_SESSION['u_notif'] = 0;
-				header("Location: ../index.php?change=successfull");
-				exit();
-            }
-            else
-            {
-                $sql = "UPDATE users SET user_notif='1' WHERE user_uid='$uid'";
-				$stmt = $conn->prepare($sql);
-				$stmt->execute();
-				$_SESSION['u_notif'] = 1;
-				header("Location: ../index.php?change=successfull");
-				exit();
-            }
-        }
-    }
-    else
-    {
-        header("Location: ../index.php?change-error");
+		if ($row)
+		{
+			if ($row['user_notif'] == 1)
+			{
+				try
+				{
+					$sql = "UPDATE users SET user_notif='0' WHERE user_uid=:username";
+					$stmt = $conn->prepare($sql);
+					$stmt->bindParam(":username", $uid);
+					$stmt->execute();
+					$_SESSION['u_notif'] = 0;
+					header("Location: ../index.php?change=successfull");
+					exit();
+				}
+				catch (PDOException $var)
+				{
+					echo $var->getMessage();
+				}	
+			}
+			else
+			{
+				try
+				{
+					$sql = "UPDATE users SET user_notif='1' WHERE user_uid=:username";
+					$stmt = $conn->prepare($sql);
+					$stmt->bindParam(":username", $uid);
+					$stmt->execute();
+					$_SESSION['u_notif'] = 1;
+					header("Location: ../index.php?change=successfull");
+					exit();
+				}
+				catch (PDOException $var)
+				{
+					echo $var->getMessage();
+				}	
+			}
+		}
+	}
+	else
+	{
+		header("Location: ../index.php?change-error");
 		exit();
-    }
+	}
 
 ?>
