5230e9c7cb73c559ad29840094a60ce136b985e2
James PIRZENTHAL <jpirzent@e4r14p7.wethinkcode.co.za>
2018-11-13 07:36:22 +0200

    upvote finished...

--- includes/upvote.inc.php
+++ includes/upvote.inc.php
@@ -8,10 +8,19 @@
 
 		$imgID = $_GET['image'];
 
-		$sql = "SELECT * FROM images WHERE image_id='$imgID'";
-		$check = $conn->prepare($sql);
-		$check->execute();
-		$row = $check->fetch(PDO::FETCH_ASSOC);
+		try
+		{
+			$sql = "SELECT * FROM images WHERE image_id=:imgID";
+			$check = $conn->prepare($sql);
+			$check->bindParam(":imgID", $imgID);
+			$check->execute();
+			$row = $check->fetch(PDO::FETCH_ASSOC);
+		}
+		catch(PDOException $e)
+		{
+			echo $sql . "<br>" . $e->getMessage();
+		}
+
 		if (!$row)
 		{
 			header("Location: ../index.php?error");
@@ -20,25 +29,58 @@
 		else
 		{
 			$liker = $_SESSION['u_uid'];
-			$sql = "SELECT COUNT(*) FROM likes WHERE likes_imgID='$imgID' AND likes_liker='$liker'";
-			$check = $conn->prepare($sql);
-			$check->execute();
-			$row = $check->fetchColumn();
+
+			try
+			{
+				$sql = "SELECT COUNT(*) FROM likes WHERE likes_imgID=:imgID AND likes_liker=:liker";
+				$check = $conn->prepare($sql);
+				$check->bindParam(":imgID", $imgID);
+				$check->bindParam(":liker", $liker);
+				$check->execute();
+				$row = $check->fetchColumn();
+			}
+			catch(PDOException $e)
+			{
+				echo $sql . "<br>" . $e->getMessage();
+			}
+
 			if ($row > 0)
 			{
-				$sql = "DELETE FROM likes WHERE likes_imgID='$imgID' AND likes_liker='$liker'";
-				$conn->exec($sql);
-				header("Location: ../view_img.php?image=".$imgID);
-				exit();
+				try
+				{
+					$sql = "DELETE FROM likes WHERE likes_imgID=:imgID AND likes_liker=:liker";
+					$check = $conn->prepare($sql);
+					$check->bindParam(":imgID", $imgID);
+					$check->bindParam(":liker", $liker);
+					$check->execute();
+					header("Location: ../view_img.php?image=".$imgID);
+					exit();
+				}
+				catch(PDOException $e)
+				{
+					echo $sql . "<br>" . $e->getMessage();
+				}	
+
 			}
 			else
 			{
 				$op = $row['image_uid'];
-				$sql = "INSERT INTO likes (likes_op, likes_liker, likes_imgID) VALUES ('$op', '$liker', '$imgID');";
-				$stmt = $conn->prepare($sql);
-				$stmt->execute();
-				header("Location: ../view_img.php?image=".$imgID);
-				exit();
+
+				try
+				{
+					$sql = "INSERT INTO likes (likes_op, likes_liker, likes_imgID) VALUES (:op, :liker, :imgID);";
+					$stmt = $conn->prepare($sql);
+					$stmt->bindParam(":op", $op);
+					$stmt->bindParam(":liker", $liker);
+					$stmt->bindParam(":imgID", $imgID);
+					$stmt->execute();
+					header("Location: ../view_img.php?image=".$imgID);
+					exit();
+				}
+				catch(PDOException $e)
+				{
+					echo $sql . "<br>" . $e->getMessage();
+				}
 			}
 		}
 	}
