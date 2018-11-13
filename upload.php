ce3a842c3bf2adcef3ca3fd97695b8cd3b7e1231
James PIRZENTHAL <jpirzent@e4r14p7.wethinkcode.co.za>
2018-11-13 08:07:42 +0200

    upload done

--- upload.php
+++ upload.php
@@ -1,6 +1,56 @@
 <?php
-	include_once 'header.php';
+	 session_start();
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<title>Camagru</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="stylesheet.css" />
+	<script src="./display-image.js" type="text/javascript"></script>
+</head>
+<body>
+	
+<header>
+	<nav>
+		<div class="header-bar">
+			<ul>
+				<li><a href="index.php">_Home_</a></li>
+				<?php
+					if (isset($_SESSION['u_id']))
+					{
+						echo '<li><a href="upload.php">_Upload_</a></li>';
+						echo '<li><a href="Webcam.php">_Webcam_</a></li>';
+					}
+				?>
+			</ul>
+			<div class="login-div">
+			<a href="user_home.php"></a>
+				<?php
+					if (isset($_SESSION['u_id']))
+					{
+						echo '<form action="includes/logout.inc.php" method="POST">
+								<button type="submit" name="submit">Logout</button>
+								<a href="user_home.php" class="user_a">User</a>
+							</form>';
+					}
+					else
+					{
+						echo '<form action="includes/login.inc.php" method="POST">
+								<input type="text" name="uid" placeholder="username">
+								<input type="password" name="pwd" placeholder="password">
+								<button type="submit" name="submit">Login</button>
+								</form>
+								<a href="forgot_pwd.php">Forgot_Password?</a>
+								<a href="signup.php">Sign Up</a>';
+					}
+				?>
+			</div>
+		</div>
+	</nav>
+</header>
 <section class="header-container">
 	<div class="header-bar">
 		<h2>Upload A Picture</h2>
@@ -8,20 +58,20 @@
 
 <div class="overlay-imgs">
 	<h1>Filters</h1>
-	<button onclick="display_img(1)"><img src="./imgs/cigarette.png" alt="cig" title="cigarette"></button>
-	<button onclick="display_img(2)"><img src="./imgs/hat.png" alt="hat" title="hat"></button>
-	<button onclick="display_img(3)"><img src="./imgs/glasses.png" alt="glasses" title="glasses"></button>
+	<button onclick="display_img(1)"><img src="./imgs/metal.png" alt="metal" title="metal"></button>
+	<button onclick="display_img(2)"><img src="./imgs/yolo.png" alt="yolo" title="yolo"></button>
+	<button onclick="display_img(3)"><img src="./imgs/emoji.png" alt="emoji" title="emoji"></button>
 </div>
 
-<form action="includes/upload.inc.php" method="post" enctype="multipart/form-data" class="upload-form">
+<div class="upload-form">
 Select an image to upload
-	<img src="./imgs/cigarette.png" alt="cig" title="cigarette" class="upload-cig" id="cig">
-	<img src="./imgs/hat.png" alt="hat" title="hat" class="upload-hat" id="hat">
-	<img src="./imgs/glasses.png" alt="glasses" title="glasses" class="upload-glasses" id="glasses">
+	<img src="./imgs/metal.png" alt="metal" title="metal" class="upload-metal" id="metal">
+	<img src="./imgs/yolo.png" alt="yolo" title="yolo" class="upload-yolo" id="yolo">
+	<img src="./imgs/emoji.png" alt="emoji" title="emoji" class="upload-emoji" id="emoji">
 	<input type="file" name="image" id="upload" onchange="displayimg(event)">
-	<img id="myimage" class="upload-img">
-	<input type="submit" name="submit" value="UPLOAD" id="img-upload">
-</form>
+	<canvas id="upload-img" class="upload-img"></canvas>
+	<input type="button" value="UPLOAD" id="img-upload" onclick="save_img()">
+</div>
 
 <div class="previous-imgs">
 	<h1>Previous Posts</h1>
@@ -29,10 +79,20 @@ Select an image to upload
 	include_once 'includes/dbh.inc.php';
 	
 	$uid = $_SESSION['u_uid'];
-	$sql = "SELECT * FROM images WHERE image_uid='$uid'";
-	$feed = $conn->prepare($sql);
-	$feed->execute();
-	$res = $feed->fetchALL(PDO::FETCH_ASSOC);
+
+	try
+	{
+		$sql = "SELECT * FROM images WHERE image_uid=:user";
+		$feed = $conn->prepare($sql);
+		$feed->bindParam(":user", $uid);
+		$feed->execute();
+		$res = $feed->fetchALL(PDO::FETCH_ASSOC);
+	}
+	catch(PDOException $e)
+	{
+		echo $sql . "<br>" . $e->getMessage();
+	}
+
 	if ($res)
 	{
 		foreach ($res as $row)
@@ -40,13 +100,16 @@ Select an image to upload
 			$imgData = $row['image_img'];
 			$imgID = $row['image_id'];
 			$img = str_replace(" ", "+", $imgData);
-			echo '<a href="view_img.php?image='.$imgID.'"><img src="data:image/png;base64,'.$img.'"></a>';
+			echo '<img src="data:image/png;base64,'.$img.'">';
 		}
 	}
 
 ?>
 
 </section>
+<div id="response">
+
+</div>
 <?php
 	include_once 'footer.php';
 ?>
